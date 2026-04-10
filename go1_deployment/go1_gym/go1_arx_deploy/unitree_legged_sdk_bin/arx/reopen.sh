ca749cf88d271ee4c433031493d2c39704b7c1df
sivan <1309519635@qq.com>
2026-04-10 21:26:57 +0800

    删除GO1

--- go1_deployment/go1_gym/go1_arx_deploy/unitree_legged_sdk_bin/arx/reopen.sh
+++
@@ -1,3 +0,0 @@
-sudo udevadm control --reload-rules && sudo udevadm trigger
-sudo slcand -o -f -s8 /dev/canable0 can0
-sudo ifconfig can0 up
--- go1_deployment/go1_gym/go1_arx_deploy/unitree_legged_sdk_bin/arx/setup_rule.sh
+++
@@ -1,2 +0,0 @@
-sudo cp arx_can.rules /etc/udev/rules.d/
-sudo chmod 777 /etc/udev/rules.d/arx_can.rules
